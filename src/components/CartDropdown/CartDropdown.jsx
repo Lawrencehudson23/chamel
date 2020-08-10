958af1c963e53158768a663ad12f92aed1f04e71
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-08-09 20:15:14 -0700

    added redux as state management

---
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CartDropdown = () => {
+    return (
+        <div className='cart-dropdown'>
+            <h1>SHOPPING BAG</h1>
+            <div className="cart-items">
+            cartItems
+            </div>
+        </div>
+    );
+}
+
+export default CartDropdown;
---
+++ src/redux/shop/shopReducer.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE={
+
+}
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'ACTION_TYPE':
+            return 
+        default:
+            return state
+    }
+}
+export default shopReducer;
